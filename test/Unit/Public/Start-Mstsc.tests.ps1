a8f8ea567f736467915813c09901ff4638ff1a2b
Omnicit <info@omnicit.se>
2019-08-08 21:04:45 +0200

    Inital commit

---
+++ test/Unit/Public/Start-Mstsc.tests.ps1
@@ -0,0 +1,8 @@
+﻿InModuleScope Omnicit {
+    Describe 'Start-Mstsc' {
+        Mock Start-Mstsc -MockWith { 'NoError' }
+        It '[Start-Mstsc][-ComputerName $Env:ComputerName] Should not throw' {
+            { Assert-VerifiableMock } | Should not throw
+        }
+    }
+}
