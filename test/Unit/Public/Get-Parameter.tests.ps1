a109d1f70e02d24827bd3648d9428680f46cd392
Omnicit <info@omnicit.se>
2019-11-07 14:20:17 +0100

    Inital commit

---
+++ test/Unit/Public/Get-Parameter.tests.ps1
@@ -0,0 +1,11 @@
+﻿InModuleScope Omnicit {
+    Describe 'Get-Parameter' {
+        It '[Get-Parameter -CommandName Get-ChildItem] Should not throw' {
+            { Get-Parameter -CommandName Get-ChildItem} | Should not throw
+        }
+        It '([(Get-Parameter -CommandName Get-ChildItem -ParameterName Path).Aliases -eq "Pa*"])' {
+            $Verify = (Get-Parameter -CommandName Get-ChildItem -ParameterName Path).Aliases -eq "Pa*"
+            $Verify | Should Be $true
+        }
+    }
+}
